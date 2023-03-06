d2441a3713304236ef1eddbda4b1e402986f6f77
mpcitmcastro <matias.x.castro@gmail.com>
2023-03-05 23:55:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { useState } from "react";
+import ButtonJSX from "./components/ButtonJSX";
+import RecipeReviewCard from "./components/RecipeReviewCard";
+import productos from "../productos";
+import Navbar from "./components/Navbar";
+
+function App() {
+  console.log(productos);
+  return (
+    <div className="container">
+      <Navbar />
+      <p></p>
+      {productos.map((producto) => (
+        <RecipeReviewCard producto={producto} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
